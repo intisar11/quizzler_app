451c4775b1589cb6f7a70e2209ec6045fb983a99
intisar11 <100384942+intisar11@users.noreply.github.com>
2022-05-23 22:33:03 -0700

    intial satge

--- quizzler-app-start/ui.py
+++ quizzler-app-start/ui.py
@@ -8,7 +8,7 @@ class UserInterface:
     def __init__(self):
 
         self.window = Tk()
-        self.window.title("Quizlerrr")
+        self.window.title("Quizlerrrr")
         self.window.config(padx=20, pady=20, bg=THEME_COLOR)
 
         self.score_label = Label("Score:0", fg="white", bg=THEME_COLOR)
