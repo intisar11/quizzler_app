1139284464d4140b04a79af23a7e316f4c594245
intisar11 <100384942+intisar11@users.noreply.github.com>
2022-05-25 11:49:17 -0700

    final

--- quizzler-app-start/ui.py
+++ quizzler-app-start/ui.py
@@ -15,13 +15,10 @@ class UserInterface:
         self.window.title("Quizlerrrr")
         self.window.config(padx= 20, pady=20, bg=THEME_COLOR)
 
-
         self.score_label = Label(text="Score:0", fg="white", bg=THEME_COLOR)
         self.score_label.grid(column=1, row=0)
 
-
         self.canvas = Canvas(width=300, height=250, bg="white")
-
 
         self.question = self.canvas.create_text(150, 125,
                                                 text="",
@@ -29,7 +26,6 @@ class UserInterface:
                                                 fill= THEME_COLOR,
                                                 font=("Ariel", 20, "italic"))
         self.canvas.grid(column=0, row=1, columnspan=2, pady= 50)
-
 
         true_image = PhotoImage(file="./images/true.png")
         false_image = PhotoImage(file="./images/false.png")
@@ -42,8 +38,6 @@ class UserInterface:
         self.get_next_question()
 
         self.window.mainloop()
-
-
 
     def true_pressed(self):
          self.give_feed_back(self.quiz.check_answer("True"))
@@ -59,11 +53,16 @@ class UserInterface:
             self.canvas.config(bg="Red")
         self.window.after(1000, self.get_next_question)
 
-
-
-
     def get_next_question(self):
-        self.canvas.config(bg="White")
-        quiz_t = self.quiz.next_question()
-        self.canvas.itemconfig(self.question, text=quiz_t)
+        self.canvas.config(bg="white")
+        if self.quiz.still_has_questions():
+            quiz_t = self.quiz.next_question()
+            self.canvas.itemconfig(self.question, text=quiz_t)
+            self.score_label.config(text=f"Score: {self.quiz.score}")
+        else:
+            self.canvas.itemconfig(self.question, text="You have ended your quiz")
+            self.true_button.config(state="disabled")
+            self.false_button.config(state="disabled")
+
+
 
